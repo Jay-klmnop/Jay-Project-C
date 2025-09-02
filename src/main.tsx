c1f3ba9082b059b52aaf7851cf8bb1d3124af7cb
Jay <jiyeyoun@outlook.com>
2025-09-03 00:43:54 +0900

    fix: Resolve MIME type error on Vercel deployment (#33)

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import App from '@/App.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='/Jay-Project-C/'>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
